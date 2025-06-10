47b40f97c97caa00a7a91646ff8bcb06b8a0578d
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-06-10 22:08:49 +0000

    Docker : Tester chaque microservice localement --> done

--- push.sh
+++ push.sh
@@ -3,6 +3,6 @@ git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
 git commit -m "
-docker-compose pour éviter de gérer chaque fichier
+Docker : Tester chaque microservice localement --> done
 "
 git push --force
