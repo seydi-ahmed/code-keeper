94683da64e82ded8c31ae9a82718771652eb3999
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-06-10 21:59:48 +0000

    docker-compose pour éviter de gérer chaque fichier

--- push.sh
+++ push.sh
@@ -3,13 +3,6 @@ git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
 git commit -m "
-Dépôt gitlab 'passed'
-1) api-gateway-app
-2) billing-app
-3) inventory-app
-4) infrastructure-config
-5) gitlab-ansible-deploy
-6) postgres-db
-7) rabbitmq
+docker-compose pour éviter de gérer chaque fichier
 "
 git push --force
