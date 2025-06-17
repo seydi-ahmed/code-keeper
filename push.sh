fce8debc3e63313014341a14171ecfe741d04974
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-06-17 16:16:10 +0000

    │ Error: Unreadable module directory │ │ Unable to evaluate directory symlink: lstat ../../modules/vpc/modules: no such file or directory ╵ ╷ │ Error: Unreadable module directory │ │ The directory  could not be read for module app at ../../modules/vpc/main.tf:22. ╵ ╷ │ Error: Unreadable module directory │ │ Unable to evaluate directory symlink: lstat ../../modules/vpc/modules: no such file or directory ╵ ╷ │ Error: Unreadable module directory │ │ The directory  could not be read for module db at ../../modules/vpc/main.tf:10. ╵ ╷ │ Error: Unreadable module directory │ │ Unable to evaluate directory symlink: lstat ../../modules/vpc/modules: no such file or directory ╵ ╷ │ Error: Unreadable module directory │ │ The directory  could not be read for module vpc at ../../modules/vpc/main.tf:1.

--- push.sh
+++ push.sh
@@ -3,6 +3,33 @@ git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
 git commit -m "
-clé
+│ Error: Unreadable module directory
+│ 
+│ Unable to evaluate directory symlink: lstat ../../modules/vpc/modules: no such file or directory
+╵
+╷
+│ Error: Unreadable module directory
+│ 
+│ The directory  could not be read for module "app" at ../../modules/vpc/main.tf:22.
+╵
+╷
+│ Error: Unreadable module directory
+│ 
+│ Unable to evaluate directory symlink: lstat ../../modules/vpc/modules: no such file or directory
+╵
+╷
+│ Error: Unreadable module directory
+│ 
+│ The directory  could not be read for module "db" at ../../modules/vpc/main.tf:10.
+╵
+╷
+│ Error: Unreadable module directory
+│ 
+│ Unable to evaluate directory symlink: lstat ../../modules/vpc/modules: no such file or directory
+╵
+╷
+│ Error: Unreadable module directory
+│ 
+│ The directory  could not be read for module "vpc" at ../../modules/vpc/main.tf:1.
 "
 git push --force
