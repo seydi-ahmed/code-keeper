93a93ef1522436ea0d13f75312ba31d91bae549e
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-06-17 16:45:53 +0000

    terraform plan -var-file=staging.tfvars

--- push.sh
+++ push.sh
@@ -3,33 +3,6 @@ git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
 git commit -m "
-│ Error: Unreadable module directory
-│ 
-│ Unable to evaluate directory symlink: lstat ../../modules/vpc/modules: no such file or directory
-╵
-╷
-│ Error: Unreadable module directory
-│ 
-│ The directory  could not be read for module "app" at ../../modules/vpc/main.tf:22.
-╵
-╷
-│ Error: Unreadable module directory
-│ 
-│ Unable to evaluate directory symlink: lstat ../../modules/vpc/modules: no such file or directory
-╵
-╷
-│ Error: Unreadable module directory
-│ 
-│ The directory  could not be read for module "db" at ../../modules/vpc/main.tf:10.
-╵
-╷
-│ Error: Unreadable module directory
-│ 
-│ Unable to evaluate directory symlink: lstat ../../modules/vpc/modules: no such file or directory
-╵
-╷
-│ Error: Unreadable module directory
-│ 
-│ The directory  could not be read for module "vpc" at ../../modules/vpc/main.tf:1.
+terraform plan -var-file="staging.tfvars"
 "
 git push --force
