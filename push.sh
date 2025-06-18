35c3a4df2279e3c8a5d5921cbf6041a369de4290
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-06-18 01:44:06 +0000

    -backend=false

--- push.sh
+++ push.sh
@@ -3,6 +3,6 @@ git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
 git commit -m "
-infrastructure staging déployée avec succès
+-backend=false
 "
 git push --force
